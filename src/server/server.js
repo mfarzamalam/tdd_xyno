2f88c4d89edae0b903d2960367eb325ce6367c56
mfarzamalam <mfarzamalam@gmail.com>
2021-09-22 16:32:22 +0500

    creating a file for server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,17 +1,19 @@
 "use strict";
 
 var http = require("http");
+var fs = require("fs");
 var server;
 
-exports.start = function(portNumber) {
+exports.start = function(htmlFileToServe, portNumber) {
 	if(!portNumber) throw "port number is required";
 
 	server = http.createServer();
-
-	server.on("request", function(request, response) {
-		response.end("Hello World");
+	server.on("request", function(request, response){
+		fs.readFile(htmlFileToServe, function (err, data) {
+			if (err) throw err;
+			response.end(data);
+		});
 	});
-
 	server.listen(portNumber);
 };
 
