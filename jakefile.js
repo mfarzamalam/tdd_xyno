c2939e6980905c73f5a03e49bbb8705b6da8a731
mfarzamalam <mfarzamalam@gmail.com>
2021-09-22 13:38:39 +0500

    modified a jakefile to create and delete directory

--- jakefile.js
+++ jakefile.js
@@ -3,12 +3,22 @@
 (function(){
     "use strict";
 
+    var TEMP_TESTFILE_DIR = "generated/test";
+
+    directory(TEMP_TESTFILE_DIR);
+
+    desc("Delete all generated files");
+    task("clean", [], function(){
+        jake.rmRf("generated");
+    });
+
+    
     desc("Build and Test");
     task("default", ["test"]);
 
 
     desc("Test Everything");
-    task("test", [], function(){
+    task("test", [TEMP_TESTFILE_DIR], function(){
         var reporter = require("nodeunit").reporters["default"];
         reporter.run(['src/server/_server_test.js'], null, function(failures){
             if (failures) fail("Test Failed");
